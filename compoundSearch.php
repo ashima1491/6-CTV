2807b27a6c4a6e547eaf34b940a607c6a1e1d92c
ashima1491 <sharma.ashima91@gmail.com>
2018-10-25 16:21:49 -0500

    migrationSoapToRest

--- compoundSearch.php
+++ compoundSearch.php
@@ -1,33 +1,48 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 /////////////////////////////////////
-/** define the SOAP client using the url for the service**/
- 
-// echo "From compoundsearch.php.";
 
-$SoapiClient = new soapclient('http://www.chemspider.com/Search.asmx?WSDL', array('trace' => 1));
 
-/*array trace helps so that we can see previous soap transactions for details see php.net documentation*/
 
-/**create an array of parameters for login **/
 
-$q_name = $_POST['compoundName']; 
 
 $query=$_POST['compoundName']; 
+$q_name = $_POST['compoundName']; 
+$thearray = array('name' => $query);
 
-$token="25798fdf-c956-4e7b-8294-4c92597cd614"; // the provided password
+$token="2P1NluuioKwal5EZPrRjv4PTfeUAAnsc"; // the provided password
 
-$thearray = array('query' => $query, 'token' => $token);
 
-/** call the service, passing the parameters and the name of the operation **/
+$curl=curl_init();
+$url="https://api.rsc.org/compounds/v1/filter/name";
 
-$result = $SoapiClient->SimpleSearch($thearray);
- 
-/** a quick test **/
+curl_setopt($curl,  CURLOPT_SSL_VERIFYPEER , false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($thearray));
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    'apikey: 2P1NluuioKwal5EZPrRjv4PTfeUAAnsc',
+    'Content-Type: application/json'
+));
+//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
-// echo "From compoundsearch.php";
+$result=curl_exec($curl);
+//print_r(curl_getinfo($curl));
+if(curl_errno($curl)){
+    echo 'Curl error: ' . curl_error($curl);
+}
+//var_dump(json_decode($result, true));
+$array=json_decode($result, true);
+//echo $array["queryId"];
 
-if (is_soap_fault($result) || ($result->SimpleSearchResult->int == null)):
+//echo" $result testing";
+//=json_decode($result, true);
+//$data = $result['response'];
+//$result = $result['response']['queryId'];
+
+
+if ( ($result == null || $result="" )):
 	echo "<h4>No Information was found on $query </h4><pre></pre>";
 	echo '<script>';
 	echo '	$(document).ready(function(){';
@@ -37,52 +52,51 @@ if (is_soap_fault($result) || ($result->SimpleSearchResult->int == null)):
 
 
 else:
+$query=$array["queryId"];
 
+  $url="https://api.rsc.org/compounds/v1/filter/".$query."/results";
+  $curl=curl_init();
+  curl_setopt($curl,  CURLOPT_SSL_VERIFYPEER , false);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'APIKEY:' .$token,
+      'Content-Type: application/json'
+  ));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  $result=curl_exec($curl);
+  $result=json_decode($result, true);
+  $recordId = $result["results"][0];
+  //echo $recordId;
+  if ( ($recordId == null || $recordId="" )):
+  echo "<h4>No Information was found on $query </h4>";
+  
+  else:
+ 
+  
+ // $recordId = http_build_query($recordId);
+  
+  $url="https://api.rsc.org/compounds/v1/records/1906/details?fields=SMILES%2CMolecularWeight%2CCommonName";
+  //var_dump($url);
+$curl=curl_init();
+curl_setopt($curl,  CURLOPT_SSL_VERIFYPEER , false);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    'APIKEY:' .$token,
+    'Content-Type: application/json'
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+$result=curl_exec($curl);
+echo $recordId;
+//print_r(curl_getinfo($curl));
 
-$somevar = $result->SimpleSearchResult->int;
+$result=json_decode($result, true);
+//echo $result;
+$SMILES = $result['smiles'];
+$commonName = $result['commonName'];
+$molecularWeight = $result['molecularWeight'];
 
-if($somevar[0] == "")
-{
- $query=$somevar;
- }
- else
- {
- $query=$somevar[0];
-  }
-
-//$query=$somevar; //the provided username
-
-$SoapiClients = new soapclient('http://www.chemspider.com/MassSpecAPI.asmx?WSDL', array('trace' => 1));
-
-$thearray = array('CSID' => $query, 'token' => $token);
-
-/** call the service, passing the parameters and the name of the operation **/
-
-//$result = $SoapiClient->GetCompoundInfo($thearray);
-
-$result = $SoapiClients->GetExtendedCompoundInfo($thearray);
-
-if (is_soap_fault($result)):
-	echo "<h1>No Information was found on: </h1>";
-
-	//echo '<pre></pre>';
-
-else:
-
-
-//$Chemspider_ID = $result->GetCompoundInfoResult->CSID;
-//$InChI = $result->GetCompoundInfoResult->InChI;
-//$InChIKey = $result->GetCompoundInfoResult->InChIKey;
-//$SMILES = $result->GetCompoundInfoResult->SMILES;
-
-
-$SMILES = $result->GetExtendedCompoundInfoResult->SMILES;
-$commonName = $result->GetExtendedCompoundInfoResult->CommonName;
-$molecularWeight = $result->GetExtendedCompoundInfoResult->MolecularWeight;
-$InChI = $result->GetExtendedCompoundInfoResult->InChI;
-//echo "<h1>Smiles = $SMILES </h1><pre>";
-//echo "<h1>InChI = $InChI </h1><pre>";
-//echo "<h1>InChIKey = $InChIKey </h1><pre>";
 
            echo '<div id="result" style="width: 100%;">';
 
@@ -107,28 +121,32 @@ $InChI = $result->GetExtendedCompoundInfoResult->InChI;
 		   echo "val=$commonName";
 		   echo '>';
 		   echo "$commonName</label>";
-		   echo "<p>InChI = $InChI </p>";
+		   //echo "<p>InChI = $InChI </p>";
 		   //*******Get image*********
 		  		   //*******Get image*********
-		   $url = "http://chemspider.com/Search.asmx/GetCompoundThumbnail?id=".$query."&token=".$token; //25798fdf-c956-4e7b-8294-4c92597cd614";
-		   // echo "URL: ". $url;
-              $ch = curl_init();
-   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-   curl_setopt($ch, CURLOPT_URL, $url);
-   //curl_setopt($ch, CURLOPT_ENCODING, $url);
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		  		   
+		   //$thearray = array('CSID' => $query, 'token' => $token);
+		   $url="https://api.rsc.org/compounds/v1/records/1906/image";
+		   $curl=curl_init();
+		   curl_setopt($curl,  CURLOPT_SSL_VERIFYPEER , false);
+		   curl_setopt($curl, CURLOPT_URL, $url);
+		   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+		       'APIKEY:' .$token,
+		       'Content-Type: application/json'
+		   ));
+		   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		  // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		   $result=curl_exec($curl);
+		   //var_dump(json_decode($result, true));
+		   $result=json_decode($result,true);
+		   $result = $result['image'];
+		   
 
-   $result = curl_exec($ch);
-
-   if (!$result) {
-     exit('cURL Error: '.curl_error($ch));
-   }
-   else{
-         $result = substr($result, 89);
-		 $result = substr($result, 0, -15);
+ 
+         //$result = substr($result, 89);
+		 //$result = substr($result, 0, -15);
 		 $compoundImage = $result;
-         //file_put_contents("images/$SMILES.png", base64_decode($result));
-		 }		 
+         		 
 		 
 		 
  
@@ -146,28 +164,14 @@ $InChI = $result->GetExtendedCompoundInfoResult->InChI;
 		   echo '<label id="submission" style="display:none;">single</label>';
 		   //
            echo '<p></p><p></p></div>';		 
-           /*                  
-		   echo '</div>';
-		   echo '<div style="float: right; width: 15%;">';
-           echo '<img src="images/';
-		   echo "$SMILES.png";
-		   echo '" style="background-color:white" alt="Smiley face">';
-		   echo '</div>';
-           echo '<p></p><p></p></div>';
-		   */
+          
 endif;
 
 endif;
 
  
 
-// see the last SOAP request
 
-//echo htmlspecialchars($SoapiClient->__getLastRequest(), ENT_QUOTES);
-
-// print the SOAP response
-
-//echo htmlspecialchars($SoapiClient->__getLastResponse(), ENT_QUOTES);
  
 /** lets store the login token just in case we want to use it for later use**/
 ?>
